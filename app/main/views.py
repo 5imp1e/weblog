86b745e5a0f6feb0935dacdcef14c4a2c58a5de7
5imp1e <luo821228649@163.com>
2016-07-19 23:29:06 +0800

    add function of delete comments

--- app/main/views.py
+++ app/main/views.py
@@ -51,10 +51,10 @@ def post(id):
     if form.validate_on_submit():
         comment = Comment(body=form.body.data,
                           post=post,
-                          #author=current_user._get_current_object()
+                          # author=current_user._get_current_object()
                           )
         db.session.add(comment)
-        flash('Your comment has been pulished.')
+        flash('Your comment has been published.')
         return redirect(url_for('.post', id=post.id, page=-1))
     page = request.args.get('page', 1, type=int)
     if page == -1:
@@ -67,6 +67,19 @@ def post(id):
     comments = pagination.items
     return render_template('post.html', posts=[post], form=form,
                            comments=comments, pagination=pagination)
+
+
+@main.route('/post/delete/<int:id>')
+def post_delete(id):
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POST_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    post = Post.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return render_template('index.html', posts=posts)
 
 
 @main.route('/moderate/enbale/<int:id>')
@@ -102,3 +115,14 @@ def moderate():
     comments = pagination.items
     return render_template('moderate.html', comments=comments,
                            pagination=pagination, page=page)
+
+
+@main.route('/moderate/delete/<int:id>/')
+@login_required
+def comments_delete(id):
+    comment = Comment.query.get_or_404(id)
+    # Comment.query.filter_by(comment=comment).delete()
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect(url_for('.moderate',
+                            page=request.args.get('page', 1, type=int)))
